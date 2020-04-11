235ea6334fd842a34e34bf14023253d4bfd95b2f
Vlad <onlytype62@yandex.ru>
2020-04-11 12:51:31 +0300

    реализация UI

--- app/src/main/java/ru/vladroid/projs/mems/screens/SplashActivity.kt
+++ app/src/main/java/ru/vladroid/projs/mems/screens/SplashActivity.kt
@@ -1,13 +1,14 @@
 package ru.vladroid.projs.mems.screens
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ru.vladroid.projs.mems.R
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setTheme(R.style.AppThemeWithoutBar)
+        setContentView(R.layout.activity_splash)
     }
 }
