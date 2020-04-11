488ab01b87e6656a5148fcb676233b198dc9cf54
Vlad <onlytype62@yandex.ru>
2020-04-11 13:05:55 +0300

    логика сплэш экрана

---
+++ app/src/main/java/ru/vladroid/projs/mems/screens/AuthActivity.kt
@@ -0,0 +1,13 @@
+package ru.vladroid.projs.mems.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.vladroid.projs.mems.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
--- app/src/main/java/ru/vladroid/projs/mems/screens/SplashActivity.kt
+++ app/src/main/java/ru/vladroid/projs/mems/screens/SplashActivity.kt
@@ -1,8 +1,11 @@
 package ru.vladroid.projs.mems.screens
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.vladroid.projs.mems.R
+import java.util.*
+
 
 class SplashActivity : AppCompatActivity() {
 
@@ -10,5 +13,16 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setTheme(R.style.AppThemeWithoutBar)
         setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                val authIntent = Intent(this@SplashActivity, AuthActivity::class.java)
+                startActivity(authIntent)
+            }
+        }, 300)
     }
 }
